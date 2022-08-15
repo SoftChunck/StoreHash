8210ceaf393f42d5b0ffa582af288bd3918075e0
Usama <softchunck@gmail.com>
2022-08-15 07:16:17 -0700

    Completed

--- src/components/Header..jsx/Header.jsx
+++ src/components/Header..jsx/Header.jsx
@@ -1,70 +1,181 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Header.css";
-import Table from 'react-bootstrap/Table';
+import Table from '../Table/Table'
+import { loadWeb3 } from "../../api";
+import {contract_abi,contract_address} from '../../Utils/contract'
 
 function Header() {
+  var [table,set_table]= new useState({
+    cols:[
+            {Header:'Index',accessor:'index'},
+            {Header:'Hash',accessor:'hash'}],
+    rows:[
+            {index:0,hash:''},
+    ]});
+
+  const [hash,sethash] = useState('');
+  const [hashindex,sethashindex] = useState('');
+  const [hashindexresult,sethashindexresult] = useState('');
+  const submit_hash = async ()=>{
+    let acc = await loadWeb3();
+    if(acc == 'No Wallet')
+    {
+      alert('No Wallet')
+    }
+    else if(acc == 'Wrong Network')
+    {
+      alert('Wrong Network')
+    }
+    else{
+      const store_contract = await new window.web3.eth.Contract(contract_abi,contract_address);
+      const owner = await store_contract.methods.owner().call();
+      if(owner == acc)
+      {
+        const returned_value = await store_contract.methods.store_hash(hash).send({from:acc});
+
+      }
+      else{
+        alert("You are Not Allowed");
+      }
+    }
+  }
+  const submit_hashindex = async ()=>{
+    let acc = await loadWeb3();
+    if(acc == 'No Wallet')
+    {
+      alert('No Wallet')
+    }
+    else if(acc == 'Wrong Network')
+    {
+      alert('Wrong Network')
+    }
+    else{
+      const store_contract = await new window.web3.eth.Contract(contract_abi,contract_address);
+      try{
+        const hashindex_data = await store_contract.methods.view_hash(hashindex).call();
+        sethashindexresult(hashindex_data);
+      }catch(e)
+      {
+        alert("Index Out of Bound");
+      }
+    }
+  }
+  const fullData = async ()=>{
+    let table_div = document.querySelector('.table_div');
+    let acc = await loadWeb3();
+    if(acc == 'No Wallet')
+    {
+      alert('No Wallet')
+    }
+    else if(acc == 'Wrong Network')
+    {
+      alert('Wrong Network')
+    }
+    else{
+      const store_contract = await new window.web3.eth.Contract(contract_abi,contract_address);
+      try{
+        const full_data = await store_contract.methods.view_fullarray().call();
+        let obj = [] ;
+        for(let x=0;x<full_data.length;x++)
+        {
+          obj.push({index:x,hash:full_data[x]})
+        }
+        console.log(obj)
+        let table = {
+          cols:[
+            {Header:'Index',accessor:'index'},
+            {Header:'Hash',accessor:'hash'}],
+          rows:obj,
+        }
+        set_table(table);
+        table_div.classList.remove('d-none');
+        table_div.classList.add('d-block');
+
+      }catch(e)
+      {
+        alert("Index Out of Bound");
+      }
+    }
+  }
+
+  const connect = async ()=>{
+    let acc = await loadWeb3();
+    if(acc == 'No Wallet')
+    {
+      alert('No Wallet')
+    }
+    else if(acc == 'Wrong Network')
+    {
+      alert('Wrong Network')
+    }
+    else{
+      console.log('Connected');
+    }
+  }
+  useEffect(()=>{
+    connect();
+  },[])
   return (
     <div className="Main_bg">
       <div className="fl">
         <div className="card card_w">
           <div className="cad-body">
-            <h5 className="h1">Log in to Your Account </h5>
+            <h5 className="h1">Submit Your Hash</h5>
             <input
               type="text"
               className="input_"
-              placeholder="Enter your name"
+              placeholder="Enter Hash"
+              onChange={(e)=>{
+                sethash(e.target.value)
+              }}
             />
+            <br />
+            <br />
+            <button className="btn btn_f " onClick={submit_hash}>Submit</button>
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="fl">
+        <div className="card card_w">
+          <div className="cad-body">
+            <h5 className="h1">Check Hash By Index</h5>
+            <input
+              type="text"
+              className="input_"
+              placeholder="Enter Index "
+              onChange={(e)=>{
+                sethashindex(e.target.value)
+              }}
+            />
+            <br />
+            <br />
+            <button className="btn btn_f " onClick={submit_hashindex}>Submit</button>
             <br />
             <input
-              type="password"
+              type="text"
               className="input_"
-              placeholder="Enter your password"
-              name=""
-              id=""
+              placeholder="Result"
+              value={hashindexresult}
             />
-            <br />
-            <button className="btn btn_f ">Login</button>
-            <br />
           </div>
         </div>
       </div>
       <div className="container">
         <div className="row">
           <div className="col-12">
-          <Table responsive>
-      <thead>
-        <tr>
-          <th>#</th>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <th key={index}>Table heading</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>2</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>3</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-      </tbody>
-    </Table>
+            <br />
+            <button className="btn btn_f "  onClick={fullData}>Show Data</button>
+                <div className="d-none table_div">
+                  <Table
+                      data={table.rows}
+                      columns={table.cols}
+                  />
+                </div>
           </div>
 
         </div>
